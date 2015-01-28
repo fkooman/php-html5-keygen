32e483127c6a06ba59711f428127b1e92e709ce9
François Kooman <fkooman@tuxed.net>
2015-01-28 11:28:37 +0100

    move cert signing also to CertManager class, update some code, support DER format for Chrom(e)(ium)

--- src/fkooman/Cert/CertManager.php
+++ src/fkooman/Cert/CertManager.php
@@ -7,15 +7,16 @@ use File_X509;
 
 class CertManager
 {
-
     const FORMAT_PEM = 0;
     const FORMAT_DER = 1;
 
-    public function __construct()
+    public function __construct($caCrt, $caKey)
     {
+        $this->caCrt = $caCrt;
+        $this->caKey = $caKey;
     }
 
-    public function generateCertificateAuthority($keySize = 2048, $commonName = 'Demo CA')
+    public static function generateCertificateAuthority($keySize = 2048, $commonName = 'Demo CA')
     {
         $r = new Crypt_RSA();
         $keyData = $r->createKey($keySize);
@@ -46,11 +47,38 @@ class CertManager
         );
     }
 
-    public function generateClientCertificate($spkac, $format = CertManager::FORMAT_PEM)
+    public function generateClientCertificate($spkac, $commonName, $serialNumber, $saveFormat = CertManager::FORMAT_PEM)
     {
-        // verify key size >= 2048
+        $caPrivateKey = new Crypt_RSA();
+        $caPrivateKey->loadKey($this->caKey);
 
-        
-        return 'pemblup/derblup';
+        $issuer = new File_X509();
+        $issuer->loadX509($this->caCrt);
+        $issuer->setPrivateKey($caPrivateKey);
+
+        $subject = new File_X509();
+        $subject->loadCA($this->caCrt);
+
+        // FIXME: verify challenge? and at least 2048 bits key!
+        $subject->loadSPKAC($spkac);
+        $subject->setDNProp('CN', $commonName);
+
+        $x509 = new File_X509();
+        // FIXME: add Subject Key Identifier???
+        // FIXME: keep track of serial and update it every time
+        // FIXME: does any other base make sense?
+        $x509->setSerialNumber($serialNumber, 10);
+
+        $result = $x509->sign($issuer, $subject, 'sha256WithRSAEncryption');
+
+        $x509->loadX509($result);
+        // https://stackoverflow.com/questions/17355088/how-do-i-set-extkeyusage-with-phpseclib
+        $x509->setExtension('id-ce-keyUsage', array('digitalSignature', 'keyEncipherment'), true);
+        $x509->setExtension('id-ce-extKeyUsage', array('id-kp-clientAuth'));
+        $x509->setExtension('id-ce-basicConstraints', array('cA' => false), true);
+        $result = $x509->sign($issuer, $x509, 'sha256WithRSAEncryption');
+
+        $format = $saveFormat === CertManager::FORMAT_PEM ? FILE_X509_FORMAT_PEM : FILE_X509_FORMAT_DER;
+        return $x509->saveX509($result, $format);
     }
 }
--- web/generate.php
+++ web/generate.php
@@ -2,56 +2,35 @@
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
+use fkooman\Ini\IniReader;
+use fkooman\Cert\CertManager;
+
+$config = IniReader::fromFile(dirname(__DIR__).'/config/config.ini');
+$storageDir = $config->v('Common', 'storageDir');
+$caDir = sprintf('%s/ca', $storageDir);
+ 
+$caCrtFile = sprintf('%s/ca.crt', $caDir);
+$caKeyFile = sprintf('%s/ca.key', $caDir);
+
+// Mozilla/5.0 (X11; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0
+// Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36
+
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+if (false !== strpos($userAgent, 'Chrome')) {
+    $format = CertManager::FORMAT_DER;
+} else {
+    $format = CertManager::FORMAT_PEM;
+}
+
 $spkac = $_POST['spkac'];
-$caCrt = dirname(__DIR__).'/data/ca/ca.crt';
-$caKey = dirname(__DIR__).'/data/ca/ca.key';
+$serialNumber = 234234;
 
-$capem = file_get_contents($caCrt);
+$commonName = bin2hex(
+    openssl_random_pseudo_bytes(16)
+);
 
-$issuer = new File_X509();
-$issuer->loadX509($capem);
-$cakey = new Crypt_RSA();
-$cakey->loadKey(file_get_contents($caKey));
-$issuer->setPrivateKey($cakey);
+$certManager = new CertManager(file_get_contents($caCrtFile), file_get_contents($caKeyFile));
+$clientCert = $certManager->generateClientCertificate($spkac, $commonName, $serialNumber, $format);
 
-$subject = new File_X509();
-$subject->loadCA($capem);
-
-// FIXME: must be >= 2048 bits!
-$subject->loadSPKAC($spkac);
-$subject->setDN('CN=WebID Client');
-
-$x509 = new File_X509();
-// FIXME: keep track of serial and update it every time
-$x509->setSerialNumber(5, 10);
-
-// FIXME: add Subject Key Identifier???
-
-
-// https://stackoverflow.com/questions/17355088/how-do-i-set-extkeyusage-with-phpseclib
-
-#        X509v3 extensions:
-#            X509v3 Authority Key Identifier:
-#                keyid:63:4D:43:5A:19:48:3F:C4:46:C1:02:BA:BF:EE:0E:E5:82:B7:66:A6
-#            X509v3 Subject Key Identifier:
-#                50:78:36:97:D7:35:E3:29:43:02:44:C9:D7:37:D7:8A:61:D7:D8:AF
-#            X509v3 Key Usage: critical
-#                Digital Signature, Key Encipherment
-#            X509v3 Basic Constraints: critical
-#                CA:FALSE
-#            X509v3 Extended Key Usage:
-#                TLS Web Client Authentication
-
-$result = $x509->sign($issuer, $subject, 'sha256WithRSAEncryption');
-
-$x509->loadX509($result);
-$x509->setExtension('id-ce-keyUsage', array('digitalSignature', 'keyEncipherment'), true);
-$x509->setExtension('id-ce-extKeyUsage', array('id-kp-clientAuth'));
-$x509->setExtension('id-ce-basicConstraints', array('cA' => false), true);
-$result = $x509->sign($issuer, $x509, 'sha256WithRSAEncryption');
-
-//echo "<pre>";
 header('Content-Type: application/x-x509-user-cert');
-// Chromium needs FILE_X509_FORMAT_DER and not PEM :(
-echo $x509->saveX509($result, FILE_X509_FORMAT_PEM);
-//echo "</pre>";
+echo $clientCert;
